a32b7707f248f35e5805ae591a11e71a1d343283
SarahWSY <sarahwongsy09@gmail.com>
2022-03-18 10:31:59 +0800

    Update comments

--- SystemCode/risk_inference.py
+++ SystemCode/risk_inference.py
@@ -1,14 +1,13 @@
-# Load Text Cleaning Pkgs
 import neattext.functions as nfx
-
 import pandas as pd
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import load_model
 import pickle
 
+# Function to preprocess dialog history and get user's risk score
 def get_risk(history):
-
+    # Getting all the user utterances as a series
     user_texts = []
     for utterance in history['dialog']:
         if utterance['speaker'] == 'usr':
@@ -20,6 +19,7 @@ def get_risk(history):
     # remove punctuations
     Clean_Texts = Clean_Texts.apply(nfx.remove_punctuations)
 
+    # Concatenating text from user utterances into 1 item to get a single prediction
     combined_user_texts = ''
     for idx, text in Clean_Texts.items():
         combined_user_texts = combined_user_texts + " " + text
@@ -29,10 +29,10 @@ def get_risk(history):
     model = load_model('model/condition_bilstm_rec_dp.h5')
 
     # loading saved tokenizer
-    tokenizer = Tokenizer()
     with open('condition.pickle', 'rb') as handle:
         tokenizer = pickle.load(handle)
 
+    # Input the combined_user_texts to the risk prediction model if it is not empty
     if combined_user_texts.size > 0:
         predictions = evaluate_text(model,tokenizer,combined_user_texts)
 
@@ -42,6 +42,7 @@ def get_risk(history):
 
     return risk_score
 
+# Function to input text (Series) into the risk prediction model and get the risk score
 def evaluate_text(model, tokenizer, text):
 
     inference_sequence = tokenizer.texts_to_sequences(text)
