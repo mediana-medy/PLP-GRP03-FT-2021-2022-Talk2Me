08fae779842fe33ae58ff903e6ac46d007f40002
mediana-medy <medy.xf@gmail.com>
2022-04-09 14:59:52 +0800

    feedback

--- SystemCode/main.py
+++ SystemCode/main.py
@@ -8,6 +8,7 @@ from SystemCode.condition.condition_inference import get_risk
 from SystemCode.problem.problem_inference import get_problem
 
 history = {}
+feedback = {}
 USER_END_PHRASES = [
     "bye",
     "thanks",
@@ -22,9 +23,9 @@ def request_handler(msg):  # directly monitor telegram
     username = msg['from']['username']
     if username not in history:
         history[username]=[]
-        history[username]['feedback']=False
+        feedback[username]=False
 
-    if msg_type == "text" and re.search(START_REGEX, str(msg['text'].lower())) and not history[username]['feedback']:
+    if msg_type == "text" and re.search(START_REGEX, str(msg['text'].lower())) and not feedback[username]:
         start = 1
         start_msg = random.choice(GREETINGS) + username + random.choice(WELCOME_MSG)
         bot.sendMessage(chat_id, str(start_msg))
@@ -35,35 +36,36 @@ def request_handler(msg):  # directly monitor telegram
             for resources in HOTLINES_LIST:
                 bot.sendMessage(chat_id, resources)
         elif user_utterances.lower() in USER_END_PHRASES :
-            if not history[username]['feedback']:
+            if not feedback[username]:
+                # Get Feedback
+                fb_resp = ""
+                bot.sendMessage(chat_id, str("Please give some feedback(1-10): "))
+                fb_resp = telepot.glance(msg)
                 # get the high/low risk score of the user (0 to 1) based on the dialogue history
+                print(fb_resp)
+
+                time.sleep(10)
                 risk_score, combined_user_texts = get_risk(history, username)
-                print("Risk Score:", risk_score)
                 # get the final condition type ['emotional','family','friendship','others','relationship','school','work']
                 problem_category = get_problem(combined_user_texts)
-                # condition_type = predict_sentiment(combined_user_texts)  # Using BERT
-                print("Condition Type:", problem_category)
                 # if user risk_score is above threshold, send additional help links
                 if risk_score > 0.5:
                     print(str(PROFESSIONAL_HELP_MSG[0]))
                     bot.sendMessage(chat_id, str(PROFESSIONAL_HELP_MSG[0]))
-                fb_resp = ""
-                # TO BE UPDATED WITH FEEDBACK
-                bot.sendMessage(chat_id, str("Feedback: "))
-                fb_resp = bot.getUpdates()
-
+                print("problem, riskscore: ", problem_category, risk_score[0], fb_resp)
                 with open("insight_data.txt", "a") as file_object:
                     # Append 'hello' at the end of file
                     data_user = str(msg['from']['username']) + ", " \
-                                + str(problem_category) + ", " + str(risk_score[0]) + ", " + fb_resp + "\n"
+                                + str(problem_category) + ", " + str(risk_score[0]) + ", " + str(fb_resp) + "\n"
                     file_object.write(data_user)
-                history[username]['feedback']=True
+                feedback[username]=True
+                history.pop(username)
 
                 end_msg = "AI: " + random.choice(END_MSG)
                 print(end_msg)
                 bot.sendMessage(chat_id, str(end_msg))
 
-        elif re.search(CHAT_REGEX, user_utterances.lower()) and not history[username]['feedback']:
+        elif re.search(CHAT_REGEX, user_utterances.lower()) and not feedback[username]:
             response = chat_conv(user_utterances, history, username)
             # response = chat_conv(user_utterances, history)
             print(response)
