e4b149c79393bbd9cf5077f57bc6f084baba6899
mediana-medy <medy.xf@gmail.com>
2022-04-11 22:58:01 +0800

    training_code_for_conversation_model

--- TrainingCode/ConversationModel/infer.py
+++ TrainingCode/ConversationModel/infer.py
@@ -1,4 +1,9 @@
 # coding=utf-8
+############################################################
+##To run infer.py
+# python infer.py --config_name strat --inputter_name strat --add_nlg_eval --seed 0 --load_checkpoint ./DATA/strat.strat/2022-03-15185126.1e-05.2.1gpu/epoch-1.bin --fp16 false --max_input_length 160 --max_decoder_input_length 40 --max_length 40 --min_length 10 --infer_batch_size 2 --infer_input_file ./_reformat/test.txt --temperature 0.7 --top_k 0 --top_p 0.9 --num_beams 1 --repetition_penalty 1 --no_repeat_ngram_size 0
+############################################################
+
 
 import argparse
 import json
